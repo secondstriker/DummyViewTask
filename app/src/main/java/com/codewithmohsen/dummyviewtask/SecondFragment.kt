eb184b7ca94a8487fc6306b7bb5158effea63733
Mohsen Sedighi <sedighi.66@gmail.com>
2021-05-22 21:51:48 +0430

    fullscreen toggle implemented.

--- app/src/main/java/com/codewithmohsen/dummyviewtask/SecondFragment.kt
+++ app/src/main/java/com/codewithmohsen/dummyviewtask/SecondFragment.kt
@@ -1,11 +1,12 @@
 package com.codewithmohsen.dummyviewtask
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -13,9 +14,11 @@ import androidx.navigation.fragment.findNavController
  */
 class SecondFragment : Fragment() {
 
+    private var isFullScreen: Boolean = false
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
@@ -27,5 +30,50 @@ class SecondFragment : Fragment() {
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        view.findViewById<Button>(R.id.button_fullscreen).setOnClickListener {
+            isFullScreen = !isFullScreen
+            toggleHideyBar()
+        }
+
+        val decorView = requireActivity().window.decorView
+        decorView.setOnSystemUiVisibilityChangeListener {
+            val height = decorView.height
+            Toast.makeText(
+                requireContext(),
+                "height dector: $height height view: ${view.height}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
+
+    /**
+     * Detects and toggles immersive mode.
+     */
+    fun toggleHideyBar() {
+        if(isFullScreen)
+            (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        else
+            (requireActivity() as AppCompatActivity).supportActionBar?.show()
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        val uiOptions = requireActivity().window.decorView.systemUiVisibility
+        var newUiOptions = uiOptions
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+
+        // Immersive mode: Backward compatible to KitKat (API 19).
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // This sample uses the "sticky" form of immersive mode, which will let the user swipe
+        // the bars back in again, but will automatically make them disappear a few seconds later.
+        newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_FULLSCREEN
+        newUiOptions = newUiOptions xor View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        requireActivity().window.decorView.systemUiVisibility = newUiOptions
+        //END_INCLUDE (set_ui_flags)
+    }
+
 }
